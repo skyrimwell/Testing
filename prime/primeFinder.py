aaef58259aaa4565e998ccc36139c85491658913
skyrimwell <51721698+skyrimwell@users.noreply.github.com>
2021-09-23 22:51:22 +0300

    ы2
    ы2

--- prime/primeFinder.py
+++ prime/primeFinder.py
@@ -1,32 +1,22 @@
 class PrimeChecker:
     def Checker(self, a):
-        # Checking that given number is more than 1
+        if a <= 0:
+            return -1
         if a > 1:
-            # Iterating over the given number with for loop
             for j in range(2, int(a/2) + 1):
-                # If the given number is divisible or not
                 if (a % j) == 0:
-                    print(a, "Не простое число")
                     return 0
-            # Else it is a prime number
             else:
-                print(a, "Простое число")
                 return 1
-        # If the given number is 1
         else:
-            print(a, "Не простое число")
             return 0
-
 
 def main():
     print("Добро пожаловать в программу проверки числа на простоту" + "\n" + "")
     a = int(input("Enter an input number:"))
-    PrimeChecker.Checker(a)
-
+    if PrimeChecker.Checker(a) == 1:
+        print(str(a) + "- простое число")
 
 if __name__ == '__main__':
     main()
-# A default function for Prime checking conditions
-# Taking an input number from the user
 
-# Printing result
--- prime/test_prime.py
+++ prime/test_prime.py
@@ -12,6 +12,15 @@ class TestPrime(unittest.TestCase):
     def test2(self):
         self.assertEqual(self.prime.Checker(21), 0)
 
+    def test3(self):
+        self.assertEqual(self.prime.Checker(-5), -1)
+
+    def test4(self):
+        self.assertEqual(self.prime.Checker(11), 1)
+
+    def test5(self):
+        self.assertEqual(self.prime.Checker(0), -1)
+
 
 if __name__ == '__main__':
     unittest.main()
